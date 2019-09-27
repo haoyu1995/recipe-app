a56368eee30ec96464a89702286de41060aac4ea
Hedy <Hedy@github.com>
2019-09-27 01:56:07 -0400

    add internationalization and Exception handle

---
+++ src/main/java/guru/springframework/recipeapp/service/ImageService.java
@@ -0,0 +1,9 @@
+package guru.springframework.recipeapp.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface ImageService {
+
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
